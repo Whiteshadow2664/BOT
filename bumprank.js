05833ab67b288884cbdd7e5b7db292fe382455ab
Whiteshadow2664 <sjemma819@gmail.com>
2025-06-20 00:17:17 +0530

    Update bumprank.js

--- bumprank.js
+++ bumprank.js
@@ -117,7 +117,7 @@ module.exports.execute = async (interaction) => {
                     .map((row, i) =>
                         `**#${i + 1}** | **${row.username}** - **Bumps:** ${row.bumps}`
                     )
-                    .join('\n') + `\n\n**Bumps** = Total number of times user has bumped`
+                    .join('\n') + `\n\n**Bumps** = Total number of times user has bumped & leaderboard updates daily at <t::R>`
             );
 
         interaction.reply({ embeds: [embed] });
