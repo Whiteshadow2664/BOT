36189030777d091fd478f09016307b5723ebc0e5
Whiteshadow2664 <sjemma819@gmail.com>
2025-06-19 13:57:44 +0530

    Update bumprank.js

--- bumprank.js
+++ bumprank.js
@@ -111,7 +111,7 @@ module.exports.execute = async (interaction) => {
 
         const embed = new EmbedBuilder()
             .setTitle('<302050872383242240> Bump Leaderboard')
-            .setColor('#98fb98')
+            .setColor('#02f5fd')
             .setDescription(
                 result.rows
                     .map((row, i) =>
